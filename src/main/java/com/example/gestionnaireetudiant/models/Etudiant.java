086703dc313ed0173501694e3ba82b49118c81ab
Lethlos <romain.rp.palayert@gmail.com>
2023-03-31 09:50:13 +0200

    Tdd Etudiant test 4 refactoring

--- src/main/java/com/example/gestionnaireetudiant/models/Etudiant.java
+++ src/main/java/com/example/gestionnaireetudiant/models/Etudiant.java
@@ -4,14 +4,11 @@ public class Etudiant {
 
 
     public Etudiant(String nom, String prenom, double moyenne) {
-        if(nom == null) {
-            throw new IllegalArgumentException("le nom ne doit pas être null");
+        if(nom == null || prenom == null) {
+            throw new IllegalArgumentException("le nom et le prenom ne doivent pas être null");
         }
         if(nom.isEmpty()) {
             throw new IllegalArgumentException("le nom ne doit pas être vide");
-        }
-        if(prenom == null) {
-            throw new IllegalArgumentException("le prenom ne doit pas être null");
         }
 
     }
